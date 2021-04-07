d3de8b538351b2e8cb947ea9603d35f5cf61de0c
heysaladin <didien.geonk@gmail.com>
2021-04-07 23:37:38 +0700

    API

--- src/App.js
+++ src/App.js
@@ -8,12 +8,29 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(function(){
+    return (async function() {
+      const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc', {
+        headers: {
+          "Content-Type": "application/json",
+          "accept": "application/json",
+          "x-api-key": process.env.REACT_APP_APIKEY
+        }
+      });
+      const {nodes} = await response.json();
+      setItems(nodes);
+    }
+    )();
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Hero />
       <Browse />
-      <Arrived />
+      <Arrived items={items} />
       <Clients />
       <Aside />
       <Footer />
